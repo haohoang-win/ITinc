a3315bae8f5517397946a028db746a27ae2fafe8
haohh-kozocom <haohh@kozo-japan.com>
2024-10-01 16:15:59 +0700

    Config font-family themesion-base-icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -6,7 +8,40 @@ module.exports = {
       colors: {
         "green-main": "#8cbc43",
       },
+      fontFamily: {
+        icons: "Themesion-Base-Icons",
+      },
+      content: {
+        bar: "\\e828",
+        close: "\\e813",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities, e, theme }) {
+      const newUtilities = {};
+
+      // Duyệt qua tất cả các giá trị trong theme.content hoặc định nghĩa thủ công
+      Object.entries(theme("content", {})).forEach(([key, value]) => {
+        newUtilities[`.${e(`base-icon-${key}`)}::before`] = {
+          content: `"${value}"`,
+          fontFamily: "Themesion-Base-Icons",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          display: "inline-block",
+          textDecoration: "inherit",
+          width: "1em",
+          marginRight: "0.2em",
+          textAlign: "center",
+          fontVariant: "normal",
+          textTransform: "none",
+          lineHeight: "1em",
+          marginLeft: "0.2em",
+          "-webkit-font-smoothing": "antialiased",
+        };
+      });
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    }),
+  ],
 };
